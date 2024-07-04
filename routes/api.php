9554e5e973302318feb6bcf00c77e60ce78dae0f
Edgar S. Ochoa <eserrano@sinprol.com.mx>
2024-07-04 00:34:20 -0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\EventController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function(Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+     ->scoped([ 'attendee' => 'event' ]); //Attendees resources are ALWAYS part of an Event. Route model binding: Laravel will automatically load it by looking for attendees of a parent event
+
