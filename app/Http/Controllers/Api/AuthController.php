df3e4299614d97457a7335ae180df521b6900928
Edgar S. Ochoa <hedka77@outlook.com>
2024-10-18 17:20:14 -0600

    avances

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,30 +5,31 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-        'email' => 'required|string|email',
-        'password' => 'required|string'
-                           ]);
+        $request->validate([ 'email'    => 'required|string|email',
+                             'password' => 'required|string' ]);
 
         $user = User::where('email', $request->email)->first();
 
-        if(!$user)
-        {
-            throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-                                                    ]);
+        if (!$user) {
+            throw ValidationException::withMessages([ 'email' => [ 'The provided credentials are incorrect.' ], ]);
         }
 
+        if (!Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([ 'email' => [ 'The provided credentials are incorrect.' ], ]);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([ 'access_token' => $token, ]);
+
     }
 
-    public function logout(Request $request)
-    {
-
-    }
+    public function logout(Request $request) {}
 }
